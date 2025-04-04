c3e290461e53a3215728799f094f012cda5b31a1
Subrata Ghosh Dastidar <srigurujoy@Subratas-Mac-mini.local>
2025-04-04 19:43:48 +0530

    add admin login template

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,48 +1,73 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-    <h1>Admin Login</h1>
-
-    <form method="POST" action="{{ route('admin.login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Admin Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>{{ __('Admin Login') }}</title>
+    <!-- CSS files -->
+    <link href="{{ asset('assets/admin/css/tabler.min.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('assets/admin/css/tabler-flags.min.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('assets/admin/css/tabler-payments.min.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('assets/admin/css/tabler-vendors.min.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('assets/admin/css/demo.min.css') }}" rel="stylesheet"/>
+    <style>
+      @import url('https://rsms.me/inter/inter.css');
+      :root {
+      	--tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+      }
+      body {
+      	font-feature-settings: "cv03", "cv04", "cv11";
+      }
+    </style>
+  </head>
+  <body  class=" d-flex flex-column">
+    <script src="./dist/js/demo-theme.min.js?1692870487"></script>
+    <div class="page page-center">
+      <div class="container container-tight py-4">
+        <div class="text-center mb-4">
+          <a href="." class="navbar-brand navbar-brand-autodark">
+            <img src="./static/logo.svg" width="110" height="32" alt="Tabler" class="navbar-brand-image">
+          </a>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Admin Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('admin.password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        <div class="card card-md">
+          <div class="card-body">
+            <h2 class="h2 text-center mb-4">{{ __('Admin Login') }}</h2>
+            <form action="{{ route('admin.login') }}" method="POST">
+                @csrf
+              <div class="mb-3">
+                <label class="form-label">{{ __('Email address') }}</label>
+                <input type="email" class="form-control" placeholder="{{ __('your@email.com') }}" autocomplete="off" name="email" value="{{ old('email') }}">
+                <x-input-error :message="$errors->first('email')" class="mt-2" />
+              </div>
+              <div class="mb-2">
+                <label class="form-label">
+                  {{ __('Password') }}
+                  <span class="form-label-description">
+                    <a href="{{ route('admin.password.request') }}">{{ __('I forgot password') }}</a>
+                  </span>
+                </label>
+                <div class="input-group input-group-flat">
+                  <input type="password" class="form-control"  placeholder="{{ __('Your password') }}"  autocomplete="off" name="password">
+                </div>
+                <x-input-error :message="$errors->first('password')" class="mt-2" />
+              </div>
+              <div class="mb-2">
+                <label class="form-check">
+                  <input type="checkbox" class="form-check-input" name="remember" />
+                  <span class="form-check-label">{{ __('Remember me on this device') }}</span>
+                </label>
+              </div>
+              <div class="form-footer">
+                <button type="submit" class="btn btn-primary w-100">{{ __('Sign in') }}</button>
+              </div>
+            </form>
+          </div>
+      </div>
+    </div>
+    <!-- Libs JS -->
+    <!-- Tabler Core -->
+    <script src="{{ asset('assets/admin/js/tabler.min.js') }}" defer></script>
+    <script src="{{ asset('assets/admin/js/demo.min.js') }}" defer></script>
+  </body>
+</html>
